0bde4ae2a6d4321ae68d5beb5efdede6e99352f8
Piyush Datta <piyush.datta@mail.utoronto.ca>
2017-04-28 08:53:39 -0400

    For pdf_to_excel_translator we now have to work with tensorflow, which can hopefully help identify the columns using a image classifier. These columns can then be inputted into the excel columns, which we can do through openpyxl application

--- python_file_translator/pdf_to_excel_translator.py
+++ python_file_translator/pdf_to_excel_translator.py
@@ -1,17 +1,13 @@
 import os
+from io import StringIO
 import openpyxl
-# from io import StringIO
-# from pdfminer.pdfparser import PDFParser, PDFDocument
-# from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-# from pdfminer.converter import PDFPageAggregator
-# from pdfminer.layout import LAParams, LTTextBox, LTTextLine
-
 import tensorflow as tf
+
+path_to_your_pdf = "G:\\Piyush\\aaSelf_Programming\\aaPython\\filetranslatorapplication\\Bell.2015.Annual_sample.pdf"
+path_to_your_pdf = "G:\\Piyush\\aaSelf_Programming\\aaPython\\filetranslatorapplication\\sample_SF424_page2.pdf"
+
 
 sess = tf.Session()
 a = tf.constant(10)
 b = tf.constant(32)
 print(sess.run(a + b))
-path_to_your_pdf = "G:\\Piyush\\aaSelf_Programming\\aaPython\\filetranslatorapplication\\Bell.2015.Annual_sample.pdf"
-path_to_your_pdf = "G:\\Piyush\\aaSelf_Programming\\aaPython\\filetranslatorapplication\\sample_SF424_page2.pdf"
-
